a07e64bf86b278ba8f1eca94252a71e5dada8200
yuki.kakihara <argentina.kyg@gmail.com>
2021-05-04 17:45:35 +0900

    リファクタ

--- src/components/GoogleAdsense.tsx
+++ src/components/GoogleAdsense.tsx
@@ -20,15 +20,17 @@ export const GoogleAdsense: React.FC<Props> = ({
   responsive = 'false',
   client,
   slot,
-}) => (
-  <ins
-    className={`${className} adsbygoogle`}
-    style={style}
-    data-ad-client={client}
-    data-ad-slot={slot}
-    data-ad-layout={layout}
-    data-ad-layout-key={layoutKey}
-    data-ad-format={format}
-    data-full-width-responsive={responsive}
-  ></ins>
-);
+}) => {
+  return (
+    <ins
+      className={`${className} adsbygoogle`}
+      style={style}
+      data-ad-client={client}
+      data-ad-slot={slot}
+      data-ad-layout={layout}
+      data-ad-layout-key={layoutKey}
+      data-ad-format={format}
+      data-full-width-responsive={responsive}
+    ></ins>
+  );
+};
