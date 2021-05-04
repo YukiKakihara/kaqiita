3b1be4124949f93f90544d2fae3fc788a5ad4c95
yuki.kakihara <argentina.kyg@gmail.com>
2021-05-04 13:35:33 +0900

    本番のみ広告が表示されるようにした

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   env: {
+    ENV: process.env.ENV,
     MICRO_CMS_API_KEY: process.env.MICRO_CMS_API_KEY,
     MICRO_CMS_HOST: process.env.MICRO_CMS_HOST,
   }
