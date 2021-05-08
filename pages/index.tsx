3ecabb8cbbed5d0696b39e2a3800f171058e4095
yuki.kakihara <argentina.kyg@gmail.com>
2021-05-08 21:53:53 +0900

    useRootを消した

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
 import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
-import Custom404 from 'pages/404';
-import { useRoot } from 'hooks/useRoot';
+import { client } from 'utils/api';
+import { BlogResponse as Blog, BlogListResponse } from 'types/blog';
 import { BlogIndex } from 'components/BlogIndex';
 import { AboutBlog } from 'components/AboutBlog';
 
-const Page: React.FC = () => {
+interface Props {
+  blogs: Blog[];
+}
+
+const Page: React.FC<Props> = ({ blogs }) => {
   const router = useRouter();
-  const { blogs, isError } = useRoot();
 
   if (router.isFallback) {
     return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <Custom404 />;
   }
 
   return (
@@ -23,6 +22,21 @@ const Page: React.FC = () => {
       <AboutBlog />
     </Wrapper>
   );
+};
+
+export const getStaticProps = async () => {
+  const key = {
+    headers: { 'X-API-KEY': process.env.MICRO_CMS_API_KEY },
+  };
+  const blogs = await client.v1.blogs
+    .$get()
+    .then((res: BlogListResponse) => res.contents)
+    .catch(() => null);
+  return {
+    props: {
+      blogs,
+    },
+  };
 };
 
 const Wrapper = styled.div({});
