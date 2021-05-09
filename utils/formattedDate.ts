f56cd05a8751768f25aaf57478a241d4eaece4f3
yuki.kakihara <argentina.kyg@gmail.com>
2021-05-09 10:52:49 +0900

    formattedDateを修正した

--- utils/formattedDate.ts
+++ utils/formattedDate.ts
@@ -2,6 +2,9 @@ export const formattedDate = (datetimeStr: string): string => {
   if (!datetimeStr) return '';
 
   const date = new Date(datetimeStr);
+  const y = date.getFullYear();
+  const m = ('00' + (date.getMonth() + 1)).slice(-2);
+  const d = ('00' + date.getDate()).slice(-2);
 
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return `${y}-${m}-${d}`;
 };
